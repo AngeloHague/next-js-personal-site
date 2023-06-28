ca666ae26b17ac6a4709c4d8063e8c9ed3303087
angelohague <angelohague@aol.co.uk>
2023-06-28 14:04:10 +0100

    Moved hero section into reusable component

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,17 @@
-import Image from 'next/image'
-// import styles from './page.module.css'
-import moon from '@/assets/Moon.svg'
-import stars from '@/assets/Stars.svg'
-import shimmer1 from '@/assets/Shimmer1.svg'
-import shimmer2 from '@/assets/Shimmer2.svg'
 import HomeSections, { HomeSectionSideNav, sectionMetadata } from '@/components/HomeSections'
+import HeroSection from '@/components/HeroSection'
 
 export default function Index() {
   return (
     <main>
       <div className="sections">
-            <section id="hero">
-                <div className="logo-container">
-                    <Image
-                      className='stars'
-                      priority
-                      src={stars}
-                      width={138}
-                      height={138}
-                      alt="Angelo Hague Moon Logo"
-                    />
-                    <div>
-                        <h1>Angelo Hague</h1>
-                        <h3>Web Developer</h3>
-                        <Image
-                        className='moon'
-                        priority
-                        src={moon}
-                        width={138}
-                        height={138}
-                        alt="Angelo Hague Moon Logo"
-                        />
-                    </div>
-                </div>
-                <div className="dive_deeper">
-                    <a href={'#'+sectionMetadata[0]}>
-                        <div>
-                          <Image
-                            priority
-                            src={shimmer1}
-                            width={193}
-                            height={54}
-                            alt="Angelo Hague shimmer image"
-                          />
-                            <h3>Dive Deeper</h3>
-                          <Image
-                            priority
-                            src={shimmer2}
-                            width={61}
-                            height={17}
-                            alt="Angelo Hague shimmer image"
-                          />
-                        </div>
-                    </a>
-                </div>
-            </section>
-            <HomeSections />
-            <HomeSectionSideNav />
-        </div>
+          <HeroSection
+            home={true}
+            btn_link={'#'+sectionMetadata[0]}
+            subtitle='Web Developer' />
+          <HomeSections />
+          <HomeSectionSideNav />
+      </div>
     </main>
   )
 }
---
+++ src/components/HeroSection.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Image from 'next/image'
+import moon from '@/assets/Moon.svg'
+import stars from '@/assets/Stars.svg'
+import shimmer1 from '@/assets/Shimmer1.svg'
+import shimmer2 from '@/assets/Shimmer2.svg'
+
+export default function HeroSection({home=false, show_moon=false, btn_text='Dive Deeper', btn_link, subtitle}) {
+    const title = (home) ? <h1>Angelo Hague</h1> : '';
+    const moon_el = (show_moon || home) ? 
+    <Image
+        className='moon'
+        priority
+        src={moon}
+        width={138}
+        height={138}
+        alt="Angelo Hague Moon Logo"
+    /> : '';
+    return (
+        <section className="home" id="hero">
+                <div className="logo-container">
+                    <Image
+                      className='stars'
+                      priority
+                      src={stars}
+                      width={138}
+                      height={138}
+                      alt="Angelo Hague Moon Logo"
+                    />
+                    <div>
+                        {title}
+                        <h3>{subtitle}</h3>
+                        {moon_el}
+                    </div>
+                </div>
+                <div className="dive_deeper">
+                    <a href={btn_link}>
+                        <div>
+                          <Image
+                            priority
+                            src={shimmer1}
+                            width={193}
+                            height={54}
+                            alt="Angelo Hague shimmer image"
+                          />
+                            <h3>{btn_text}</h3>
+                          <Image
+                            priority
+                            src={shimmer2}
+                            width={61}
+                            height={17}
+                            alt="Angelo Hague shimmer image"
+                          />
+                        </div>
+                    </a>
+                </div>
+            </section>
+    )
+}
