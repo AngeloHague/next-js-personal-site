9efb96979ee03a57399fffca85a5b4646d08f484
angelohague <angelohague@aol.co.uk>
2023-07-03 16:38:39 +0100

    Contact page setup

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,38 @@
+import HeroSection from "@/components/HeroSection";
+import { SideNav } from "@/components/SideNav";
+import styles from './page.module.css'
+import { quicksand } from "@/lib/fonts";
+
+export const metadata = {
+  title: 'Contact | Angelo Hague',
+  description: 'Here are all the ways you can get in touch with me, including a nice little form below.',
+}
+
+
 export default function ContactPage() {
     return (
-      <main className="page">
-        <h1>Contact Me</h1>
+      <main>
+        <div className="sections">
+          <HeroSection
+            subtitle='Find me on'
+            btn_text='Or use this form'
+            btn_link={'#form'}
+          />
+          <SideNav slugs={['#form']} />
+          <section>
+            <h2>Contact Form</h2>
+            <form className={styles.contact} name="contact" id='form' method="POST" data-netlify="true">
+                <label>Your Name: <input className={quicksand.className} type="text" name="name" /></label>
+                <label>Your Email: <input className={quicksand.className} type="email" name="email" /></label>
+                <label>Your Role: <select className={quicksand.className} name="role[]">
+                  <option value="Customer">Customer</option>
+                  <option value="Employer">Employer</option>
+                </select></label>
+                <label>Message: <textarea className={quicksand.className} name="message"></textarea></label>
+                <button type="submit">Send</button>
+            </form>
+          </section>
+      </div>
       </main>
     );
   }
