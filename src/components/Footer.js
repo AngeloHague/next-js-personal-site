e05a26e89120404a136645292ce7c679ae82b799
angelohague <angelohague@aol.co.uk>
2023-07-10 10:30:54 +0100

    Added page footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer> <small>&copy; Copyright 2023, Angelo Hague. All Rights Reserved.</small> </footer>
+    )
+}
