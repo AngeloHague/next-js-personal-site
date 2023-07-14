58060f7ae9b9487373ad9815ea40ce696b17e586
angelohague <angelohague@aol.co.uk>
2023-07-14 10:39:14 +0100

    Changed home sections to use json format

--- src/components/HomeSections.js
+++ src/components/HomeSections.js
@@ -8,16 +8,33 @@ import KeyframeAnimation from './KeyframeAnimation';
 
 export const sectionMetadata = getSectionMetadata();
 
+const animations = {
+    lincoln_cathedral: (<LineArt />),
+    responsive_design: (<KeyframeAnimation json={responsive} />)
+}
+
 export default function HomeSections({svg})
 {
     // const sectionMetadata = getSectionMetadata();
-    const sections = sectionMetadata.map(async(slug) => (
+    const sections = sectionMetadata.map(async(slug) => {
+        let section = getSection(slug)
+        let html = Object.keys(section.contents).map((key) => {
+            let subsec = section.contents[key]
+            return Object.keys(subsec).map((k) => {
+                if (k=='markdown') return <p><Markdown>{subsec[k]}</Markdown></p>
+                else if (k='animation') return animations[subsec[k]]
+            })
+        })
+        return (
         <section id={slug} className='home_section'>
-            <div className="content"><Markdown>{getSection(slug)}</Markdown></div>
+            <div className="content">
+                <h2>{section.title}</h2>
+                {html}
+            </div>
             {/* <Image src={CathedralSVG} width='800' height='800' /> */}
             {/* <LineArt svg={svg} /> */}
-            <KeyframeAnimation json={responsive} />
+            {/* <KeyframeAnimation json={responsive} /> */}
         </section>
-    ));
+    )});
     return <>{sections}</>;
 }
--- src/lib/home_sections.js
+++ src/lib/home_sections.js
@@ -1,20 +1,30 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import sections from '@/data/index/sections.json'
+import Markdown from 'markdown-to-jsx';
 
 const SectionsDirectory = path.join(process.cwd(), 'src', 'data','home_sections');
 
-export const getSection = (slug) => {
-  const file = path.join(SectionsDirectory, slug+'.md');
-  const content = fs.readFileSync(file, 'utf8');
-  // use markdown to separate metadata
-  const matterResults = matter(content);
-  return matterResults.content;
+export const getSectionMetadata = () => {
+  return(Object.keys(sections))
 }
 
-export const getSectionMetadata = () => {
-  const files = fs.readdirSync(SectionsDirectory);
-  const mdSections = files.filter((file) => file.endsWith('.md'));
-  const slugs = mdSections.map((file) => file.replace('.md', ''));
-  return slugs;
+export const getSection = (slug) => {
+  return sections[slug]
 }
+
+// export const getSection = (slug) => {
+//   const file = path.join(SectionsDirectory, slug+'.md');
+//   const content = fs.readFileSync(file, 'utf8');
+//   // use markdown to separate metadata
+//   const matterResults = matter(content);
+//   return matterResults.content;
+// }
+
+// export const getSectionMetadata = () => {
+//   const files = fs.readdirSync(SectionsDirectory);
+//   const mdSections = files.filter((file) => file.endsWith('.md'));
+//   const slugs = mdSections.map((file) => file.replace('.md', ''));
+//   return slugs;
+// }
