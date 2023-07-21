5a5f529ac62df829c28d5fe8a06f98d273b25dcd
angelohague <angelohague@aol.co.uk>
2023-07-21 22:42:06 +0100

    Styled submission page

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,9 +1,15 @@
+import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
 export default function Sucess() {
     return (
-        <div>
-            Form submitted successfully.
+        <div className='sections'>
+            <HeroSection
+                subtitle='Submission successful'
+                low_moon
+                btn_text='Click to go back'
+                btn_link='/'
+            />
         </div>
     )
 }
