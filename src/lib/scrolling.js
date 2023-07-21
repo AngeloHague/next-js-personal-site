ae96875fe86debf4d1d2452f8b8fb7a6ee221281
angelohague <angelohague@aol.co.uk>
2023-07-21 22:42:27 +0100

    Made scroll-handler check for #

--- src/lib/scrolling.js
+++ src/lib/scrolling.js
@@ -1,10 +1,13 @@
 'use client'
 export const handleScroll = (e) => {
-    e.preventDefault();
     const href = e.currentTarget.href;
-    const target = href.replace(/.*\#/, "");
-    const elem = document.getElementById(target);
-    elem?.scrollIntoView({
-        behavior: 'smooth',
-    });
+    let target = href.replace(/.*\//, "");
+    if (target.startsWith('#')) {
+        e.preventDefault();
+        target = target.replace('#', "");
+        const elem = document.getElementById(target);
+        elem?.scrollIntoView({
+            behavior: 'smooth',
+        });
+    } 
 };
