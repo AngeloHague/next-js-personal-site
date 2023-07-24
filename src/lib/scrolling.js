a6ba5ef2b29b989e93a80dd4400a2daef2f6db1c
angelohague <angelohague@aol.co.uk>
2023-07-24 11:31:25 +0100

    Fixed scrolling js on subpages

--- src/lib/scrolling.js
+++ src/lib/scrolling.js
@@ -1,13 +1,16 @@
 'use client'
-export const handleScroll = (e) => {
-    const href = e.currentTarget.href;
-    let target = href.replace(/.*\//, "");
-    if (target.startsWith('#')) {
+export function handleScroll(e) {
+    // get href attr
+    const href = e.currentTarget.getAttribute('href');
+    // check if href starts with #
+    if (href.startsWith('#')) {
+        // prevent default behaviour
         e.preventDefault();
-        target = target.replace('#', "");
+        // replace hashtag and scroll the element with matching id
+        const target = href.replace('#', "");
         const elem = document.getElementById(target);
         elem?.scrollIntoView({
             behavior: 'smooth',
         });
-    } 
+    }
 };
