fec75631882c1287ae549bacb8baa8d76687ea95
angelohague <angelohague@aol.co.uk>
2023-08-30 16:13:51 +0100

    Redesigned hero to allow children

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import ShimmerAnimation from './ShimmerAnimation';
 import styles from './styles/hero.module.scss'
 import starSass from './styles/stars.module.sass'
 
-export default function HeroSection({home=false, low_moon=false, full_space=false, btn_text='Dive Deeper', btn_link, subtitle, requireJS=false, back_link, stretchHero=false, children}) {
+export default function HeroSection({home=false, low_moon=false, full_space=false, btn_text='Dive Deeper', btn_link, subtitle, requireJS=false, back_link, containerClasses, children}) {
     const go_back = (back_link) ? <><a className='back_link' href={back_link}><h4> &lt; Go Back</h4></a></> : <></>;
     const moonClass = (low_moon) ? [styles.moon, styles.low].join(' ') : styles.moon;
     const title = (home) ? <h1>Angelo Hague</h1> : '';
@@ -38,7 +38,7 @@ export default function HeroSection({home=false, low_moon=false, full_space=fals
     /> : '';
     return (
         <section className={(home) ? [styles.hero,styles.home].join(' ') : styles.hero} id='hero'>
-          <div className={styles.space}>
+          <div className={(full_space) ? [styles.space,styles.full].join(' ') : styles.space }>
               <div className={styles.stars}>
                 <div className={styles.container}>
                   <div id={starSass.stars} />
@@ -46,10 +46,10 @@ export default function HeroSection({home=false, low_moon=false, full_space=fals
                   <div id={starSass.stars3} />
                 </div>
               </div>
-              <div className={styles.heroContent}>
+              <div className={[styles.heroContent,containerClasses].join(' ')}>
                   {go_back}
                   {title}
-                  <h3>{subtitle}</h3>
+                  {subtitle ? <h3>{subtitle}</h3> : null}
                   {moon_el}
                   {children}
               {noJS}
