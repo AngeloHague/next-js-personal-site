9a8e08da5172611e78429ce815ef8fee7ec5a9f3
angelohague <angelohague@aol.co.uk>
2023-09-14 11:06:26 +0100

    Longer meta description for google SEO

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { SideNav } from '@/components/SideNav'
 
 export const metadata = {
   title: 'Angelo Hague Web Development',
-  description: 'Computer Science Graduate and Web Developer',
+  description: 'Derbyshire and South Yorkshire based Freelance Web Developer. Get affordable responsive website design and development in Shopify, WordPress, React and more!',
   keywords: ['responsive web design', 'wordpress web design', 'junior web developer', 'web programming', 'junior front end developer', 'ecommerce website development', 'web developer', 'web design near me', 'front end developer', 'front end web developer', 'shopify wesite design'].join(', '),
 }
 
